c3fe5d04565a97b368b35ddbcaaed4fc428e51d9
aamir2617 <aamirvahora1828@gmail.com>
2020-06-18 15:46:47 +0530

    login and sign up with password complete

--- app/src/main/java/com/example/nadiadmartadmin/homeScreenActivity/HomeScreenActivity.java
+++ app/src/main/java/com/example/nadiadmartadmin/homeScreenActivity/HomeScreenActivity.java
@@ -1,14 +1,10 @@
 package com.example.nadiadmartadmin.homeScreenActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.example.nadiadmartadmin.R;
-import com.example.nadiadmartadmin.signInActivity.SignInActivity;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeScreenActivity extends AppCompatActivity {
@@ -24,16 +20,8 @@ public class HomeScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
-        ConstraintLayout layout = findViewById(R.id.layout);
 
-        layout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mAuth.signOut();
-                startActivity(new Intent(HomeScreenActivity.this, SignInActivity.class));
 
-            }
-        });
 
 
 
